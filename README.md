9af74f7a5267b5bd6cd2b122ce9b088741381e2a
Ivon <ivonne@jjf.com>
2021-11-20 09:51:21 -0600

    working on Modals

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MyTech-Blog
+
+npm init
+npm install express sequelize mysql2
+npm install dotenv
+npm install bcrypt
+npm install express-handlebars
+npm i express-session connect-session-sequelize
---
+++ models/index.js
@@ -0,0 +1,38 @@
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
+
+// create associations between user and post
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+//create associations between comments and users over a post
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote, Comment };
